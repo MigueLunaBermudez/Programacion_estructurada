f8e36f7e463d4daba296f9abd5bcc3cfa91e2fd9
MigueLunaBermudez <miguel_3141240132@utd.edu.mx>
2025-07-21 21:19:14 -0600

    commit3

---
+++ p3/4_proyecto_notas/main.py
@@ -0,0 +1,85 @@
+import funciones
+from usuarios import usuario
+
+def main():
+    opcion=True
+    while opcion:
+        funciones.borrarPantalla()
+        opcion=funciones.menu_usurios()
+
+        if opcion=="1" or opcion=="REGISTRO":
+            funciones.borrarPantalla()
+            print("\n \t ..:: Registro en el Sistema ::..")
+            nombre=input("\t ¿Cual es tu nombre?: ").upper().strip()
+            apellidos=input("\t ¿Cuales son tus apellidos?: ").upper().strip()
+            email=input("\t Ingresa tu email: ").lower().strip()
+            password=input("\t Ingresa tu contraseña: ").strip()
+            #Agregar codigo
+            regresar=usuario.registrar(nombre,apellidos,email, password)
+            if regresar: 
+                print("{nombre} {apellido} se registro correctamente con el e-mail {email}")
+            else:
+                print("por favor intentelo de nuevo ... no fue posible realizar el registro en este momento...")              
+            funciones.esperarTecla()
+        elif opcion=="2" or opcion=="LOGIN": 
+            funciones.borrarPantalla()
+            print("\n \t ..:: Inicio de Sesión ::.. ")     
+            email=input("\t Ingresa tu E-mail: ").lower().strip()
+            password=input("\t Ingresa tu contraseña: ").strip()
+            #Agregar codigo 
+            lista_usuario=usuario.iniciar_secion(email, password)
+            if lista_usuario:
+                menu_notas(lista_usuario[0], lista_usuario[1], lista_usuario[2])
+            else:
+                print("El E-mail y/o Contraseña son inorrectos... Por favor intentelo nuevamente")
+            funciones.esperarTecla()
+        elif opcion=="3" or opcion=="SALIR": 
+            print("Termino la Ejecución del Sistema")
+            opcion=False
+            funciones.esperarTecla()  
+        else:
+            print("Opcion no valida")
+            opcion=True
+            funciones.esperarTecla() 
+
+def menu_notas(usuario_id,nombre,apellidos):
+    while True:
+        funciones.borrarPantalla()
+        print(f"\n \t \t \t Bienvenido {nombre} {apellidos}, has iniciado sesión ...")
+        opcion=funciones.menu_notas()
+
+        if opcion == '1' or opcion=="CREAR":
+            funciones.borrarPantalla()
+            print(f"\n \t .:: Crear Nota ::. ")
+            titulo=input("\tTitulo: ")
+            descripcion=input("\tDescripción: ")
+            #Agregar codigo
+            funciones.esperarTecla()    
+        elif opcion == '2' or opcion=="MOSTRAR":
+            funciones.borrarPantalla()
+            #Agregar codigo  
+            funciones.esperarTecla()
+        elif opcion == '3' or opcion=="CAMBIAR":
+            funciones.borrarPantalla()
+            print(f"\n \t .:: {nombre} {apellidos}, vamos a modificar un Nota ::. \n")
+            id = input("\t \t ID de la nota a actualizar: ")
+            titulo = input("\t Nuevo título: ")
+            descripcion = input("\t Nueva descripción: ")
+            #Agregar codigo
+            funciones.esperarTecla()      
+        elif opcion == '4' or opcion=="ELIMINAR":
+            funciones.borrarPantalla()
+            print(f"\n \t .:: {nombre} {apellidos}, vamos a borrar un Nota ::. \n")
+            id = input("\t \t ID de la nota a eliminar: ")
+            #Agregar codigo
+            funciones.esperarTecla()    
+        elif opcion == '5' or opcion=="SALIR":
+            break
+        else:
+            print("\n \t \t Opción no válida. Intenta de nuevo.")
+            funciones.esperarTecla()
+
+if __name__ == "__main__":
+    main()    
+
+
