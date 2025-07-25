13c02756b01ba3d97c36aa23022a17f339f6782b
MigueLunaBermudez <miguel_3141240132@utd.edu.mx>
2025-07-25 14:13:37 -0600

    Delete p1 directory

--- p1/2_modulos_paquetes/utilizar_paquetes.py
+++
@@ -1,9 +0,0 @@
-from paquete1 import modulos
-
-print(modulos.saludar("Daniel Contreras Ruano"))
-
-modulos.borrarPantalla()
-nom,tel =modulos.solicitarDatos2()
-print(f"\n\t.::Agenda Telefonica::. \n\t\tNombrer {nom} \n\tTelefono:  {tel}")
-modulos.espereTecla
-
